2c6209d66cb0e33f8bc268890f1e6014fdc0e6dd
Ayanadiya <ayanadiya17@gmail.com>
2024-09-10 15:18:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const mainheading=document.getElementById('main-heading');
+mainheading.textContent='Fruit World';
+mainheading.style.color='orange';
+
+const header=document.getElementById('header');
+header.style.backgroundColor='green';
+header.style.borderBottom='3px solid orange';
+
+const baskethead=document.getElementById('basket-heading');
+baskethead.style.color='green';
+
+const thank=document.getElementById('thanks');
+thank.innerHTML='<p>Please visit us again</p>';
