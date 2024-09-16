528d66e0e6a4aa61924ca499b7403ef513209642
Ayanadiya <ayanadiya17@gmail.com>
2024-09-16 18:51:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 
-const lists=document.querySelectorAll('li');
-lists.forEach((li) => {
-    const edit=document.createElement('button');
-    edit.className='edit-btn';
-    edit.innerText='Edit';
-    li.appendChild(edit);
-});
-const form=document.querySelector('form');
-const fruits=document.querySelector('.fruits');
-form.addEventListener('submit',function(event){
-    event.preventDefault;
-    const fruitToAdd=document.getElementById("fruit-to-add");
-    const newLi=document.createElement('li');
-    const newLiText=document.createTextNode(fruitToAdd.value);
-    newLi.className='fruit';
-    newLi.appendChild(newLiText);
-    const dltbtn =document.createElement('button');
-    dltbtn.innerText='x';
-    dltbtn.className='delete-btn';
-    newLi.appendChild(dltbtn);
-    const edit=document.createElement('button')
-    edit.innerText='Edit';
-    edit.className='edit-btn';
-    newLi.appendChild(edit);
-    fruits.appendChild(newLi);
-    console.log(newLi);
-})
-fruits.addEventListener('click',function(event){
-    if(event.target.classList.contains('delete-btn')){
-        const fruitToDelete=event.target.parentElement;
-        fruits.removeChild(fruitToDelete);
+function handleFormSubmit(event){
+    event.preventDefault();
+    const username=event.target.username.value;
+    const email=event.target.email.value;
+    const phone=event.target.phone.value;
+    const userdetails={
+      email,
+      phone,
+      username,
     }
-})
+    const userdetails_serialized=JSON.stringify(userdetails);
+    localStorage.setItem("User Details",userdetails_serialized);
+    const list=document.querySelector('ul');
+    const newLi=document.createElement('li');
+    newLi.textContent=username+"-"+email+"-"+phone
+    const dltbtn=document.createElement('button')
+    dltbtn.type="button"
+    dltbtn.className='delete-btn';
+    dltbtn.textContent="Delete";
+    dltbtn.onclick=()=>{
+      localStorage.getItem(event.email);
+      list.removeChild(newLi);
+    }
+    newLi.appendChild(dltbtn);
+    list.appendChild(newLi);
+};
+module.exports=handleFormSubmit;
