610770e74f2c2902efd9570260d2bd06fe6edbac
Ayanadiya <ayanadiya17@gmail.com>
2024-09-24 14:16:27 +0530

    Expense Tracker using bootstrap

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-
-const lists=document.querySelectorAll('li');
-lists.forEach((li) => {
-    const edit=document.createElement('button');
-    edit.className='edit-btn';
-    edit.innerText='Edit';
-    li.appendChild(edit);
-});
-const form=document.querySelector('form');
-const fruits=document.querySelector('.fruits');
-form.addEventListener('submit',function(event){
-    event.preventDefault;
-    const fruitToAdd=document.getElementById("fruit-to-add");
-    const newLi=document.createElement('li');
-    const newLiText=document.createTextNode(fruitToAdd.value);
-    newLi.className='fruit';
-    newLi.appendChild(newLiText);
-    const dltbtn =document.createElement('button');
-    dltbtn.innerText='x';
+const form=document.getElementById('expense');
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    const list=document.querySelector('ul');
+    const newli=document.createElement('li');
+    newli.className="list-group-item";
+    const expenseEvent=event.target.expenseevent.value;
+    const amount=event.target.amount.value;
+    const type=event.target.expensetype.value;
+    let Expensedetails={
+        expenseEvent,
+        amount,
+        type,
+      };
+        let Expensedetails_serialized=JSON.stringify(Expensedetails);
+      localStorage.setItem("ExpenseDetails",Expensedetails_serialized);
+    newli.textContent=expenseEvent+"-"+amount+"-"+type
+    const dltbtn=document.createElement('button')
+    dltbtn.type="button"
     dltbtn.className='delete-btn';
-    newLi.appendChild(dltbtn);
-    const edit=document.createElement('button')
-    edit.innerText='Edit';
-    edit.className='edit-btn';
-    newLi.appendChild(edit);
-    fruits.appendChild(newLi);
-    console.log(newLi);
-})
-fruits.addEventListener('click',function(event){
-    if(event.target.classList.contains('delete-btn')){
-        const fruitToDelete=event.target.parentElement;
-        fruits.removeChild(fruitToDelete);
-    }
+    dltbtn.textContent="Delete";
+    dltbtn.onclick=()=>{
+        localStorage.removeItem("ExpenseDetails");
+        list.removeChild(newli);
+      }
+    newli.appendChild(dltbtn);
+    const editbtn=document.createElement('button');
+    editbtn.type="button"
+    editbtn.textContent="Edit";
+    editbtn.onclick=()=>{
+        document.getElementById('expenseevent').value=Expensedetails.expenseEvent;
+        document.getElementById('amount').value=Expensedetails.amount;
+        document.getElementById('expensetype').value=Expensedetails.type;
+        localStorage.removeItem(event.expenseEvent);
+        list.removeChild(newli);
+      }
+    newli.appendChild(editbtn);
+    list.appendChild(newli);
+    document.getElementById('expenseevent').value = '';
+    document.getElementById('amount').value = '';
+    document.getElementById('expensetype').selectedIndex = 0;
+  
 })
